c6578958646fed8d5f7982082e3cf88dd1f0a8c4
Andy Brice <andybrice@users.noreply.github.com>
2018-02-28 19:57:24 +0000

    Added long description

--- setup.py
+++ setup.py
@@ -8,4 +8,34 @@ setup(name='pypework',
       author_email='andybrice@users.noreply.github.com',
       license='MIT',
       packages=['pypework'],
-      zip_safe=False)
+      zip_safe=False,
+      long_description=
+      """
+      A library to add a neat, readable, functional pipeline syntax to Python.
+
+      Rewrite messy nested function calls such as:
+
+      It allows you to rewrite messy nested function calls such as this:
+
+      .. code:: python
+
+         title_sanitized =
+           replace(replace(replace(lowercase("Lorem Ipsum Dolor 2018/02/18"), " ", "_"), "/", "-"), "@", "at")
+
+         title_sanitized # -> "lorem_ipsum_dolor_2018-02-18"
+
+      In a far more readable format like this:
+
+      .. code:: python
+
+         title_sanitized = (
+           "Lorem Ipsum Dolor 2018/02/18"
+             >> f.lowercase
+             >> f.replace("/", "-")
+             >> f.replace(" ", "_")
+             >> f.replace("@", "at")
+         )
+
+         title_sanitized # -> "lorem_ipsum_dolor_2018-02-18"         
+      """
+      )
