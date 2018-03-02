39c3bfa02f90d2b34e37b8c15db0d2d454839788
Andy Brice <andybrice@users.noreply.github.com>
2018-03-02 00:43:59 +0000

    Removed prototype code from version control

--- Prototype/example_functions.py
+++
@@ -1,4 +0,0 @@
-def increment(x): return x + 1
-def double(x): return x * 2
-def add(a,b): return a + b
-def multadd(a,b,c): return (a * b) + c
--- Prototype/frame_test.py
+++
@@ -1,6 +0,0 @@
-import sys
-sys.path.append('../')
-import pypework
-
-f = pypework.FunctionCatcher()
-f._identifier_chain
