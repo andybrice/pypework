ce89021a9650bdea4ff2c5a31a250756998bba96
Andy Brice <andybrice@users.noreply.github.com>
2018-03-19 10:41:12 +0000

    Started redesign to handle method calls and subscripts

--- pypework/__init__.py
+++ pypework/__init__.py
@@ -74,25 +74,109 @@ class InputPlaceholder: pass
 
 ____ = InputPlaceholder()
 
+# class MethodCatcher:
+#     def __init__(self, scope=[]):
+#         self._scope = scope
+
+#     def __getattr__(self, name):
+#         new_scope = [*self._scope, name]
+#         return MethodCatcher(scope=new_scope)
+
+#     def invoke_on(self, other):
+#         def resolve_identifier_chain(root_object, chain):
+#                 if len(chain) == 1:
+#                     return getattr(root_object, chain[0])
+#                 else:
+#                     head = chain[0]
+#                     tail = chain[1:]
+#                     sub_object = getattr(root_object, head)
+#                     return resolve_identifier_chain(sub_object, tail)
+
+#         return resolve_identifier_chain(other, self._scope)
+
+def invoke_deffered_chain(input_object, chain):
+    if len(chain) == 1:
+        return chain[0].invoke_on(input_object)
+    else:
+        head = chain[0]
+        tail = chain[1:]
+        sub_object = head.invoke_on(input_object)
+        return invoke_deffered_chain(sub_object, tail)
+
+class DefferredAttribute:
+    def __init__(self, attribute_name):
+        self.attribute_name = attribute_name
+
+    def invoke_on(self, input):
+        return getattr(input, self.attribute_name)
+
+    def __repr__(self):
+        return f"DefferredAttribute: .{self.attribute_name}"
+
+class DefferredMethodCall:
+    def __init__(self, *arguments, **keywords):
+        self.arguments = arguments
+        self.keywords = keywords
+
+    def invoke_on(self, input):
+        return input(*self.arguments, **self.keywords)
+
+    def __repr__(self):
+        return f"DefferredMethodCall: {self.arguments} {self.keywords}"
+
+class DefferredSubscript:
+    def __init__(self, key):
+        self.key = key
+
+    def invoke_on(self, input):
+        return input.__getitem__(self.key)
+
+    def __repr__(self):
+        return f"DefferredSubscript: {self.key}"
+
+class ChainCatcher:
+    def __init__(self, chain=[]):
+        self._chain = chain
+
+    def __add_link__(self, new_link):
+        new_chain = [*self._chain, new_link]
+        return ChainCatcher(chain = new_chain)
+
+    def __getattr__(self, attribute):
+        return self.__add_link__( DefferredAttribute(attribute) )
+
+    def __getitem__(self, key):
+        return self.__add_link__( DefferredSubscript(key) )
+
+    def __call__(self, *args, **kwargs):
+        return self.__add_link__( DefferredMethodCall(*args, **kwargs) )
+
+    def __repr__(self):
+        return f"ChainCatcher: {self._chain}"
+
 class MethodCatcher:
-    def __init__(self, scope=[]):
-        self._scope = scope
+    def __init__(self, scope=None):
+        self._identifier = scope
 
     def __getattr__(self, name):
-        new_scope = [*self._scope, name]
-        return MethodCatcher(scope=new_scope)
+        return MethodCatcher(scope=name)
 
-    def invoke_on(self, other):
-        def resolve_identifier_chain(root_object, chain):
-                if len(chain) == 1:
-                    return getattr(root_object, chain[0])
-                else:
-                    # root_object = bob
-                    # chain = ['date_of_birth', 'day']
-                    head = chain[0]
-                    tail = chain[1:]
-                    sub_object = getattr(root_object, head)
-                    return resolve_identifier_chain(sub_object, tail)
+    def invoke_on(self, root_object):
+        return getattr(root_object, self._identifier)
 
-        return resolve_identifier_chain(other, self._scope)
+# class DeferredMethodAccessor:
+    # def execute():
+    #     precursor_result = self._precursor.invoke_on(root_object)
+
+
+#     def invoke_on(self, root_object):
+#         if self._prerequisite:
+#             self._prerequisite.invoke_on(root_object)
+#         else:
+
+# class DeferredSubscriptAccessor:
+#     def invoke_on(self, root_object):
+#         if self._prerequisite:
+#             self._prerequisite.invoke_on(root_object)
+#         else:
 
